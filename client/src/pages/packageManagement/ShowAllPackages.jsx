007cf81e8a0c38d40e1051209f2beacda4880098
DamikaDeshan <damikadeshan40@gmail.com>
2024-10-08 16:20:34 +0530

    update

--- client/src/pages/packageManagement/ShowAllPackages.jsx
+++ client/src/pages/packageManagement/ShowAllPackages.jsx
@@ -3,6 +3,7 @@ import { motion } from "framer-motion";
 import { FaBox, FaChartLine, FaSearch } from "react-icons/fa";
 import axios from "axios";
 import Swal from "sweetalert2";
+import Modal from "react-modal";
 import { useNavigate } from "react-router-dom";
 import { PDFDownloadLink } from "@react-pdf/renderer";
 import PackageReport from "./PackageReport"; // Import the new component
@@ -12,6 +13,10 @@ const ShowAllPackages = () => {
   const [maintancePkgs, setmaintancePkgs] = useState([]);
   const [searchValue, setSearchValue] = useState("");
   const [filteredPkg, setFilteredPkg] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [sparePart, setSparePart] = useState([]); // Added state for spare parts
+  const [pickList, setPickList] = useState([]); // Added state for pickList
+  const [item, setItem] = useState({ name: "", quantity: 0 }); // Added state for item
 
   const handleUpdateClick = (id) => {
     navigate(`/admin/upd/${id}`);
@@ -68,9 +73,63 @@ const ShowAllPackages = () => {
     }
   };
 
+  useEffect(() => {
+    const fetchModifications = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:3000/api/mod/getMod"
+        );
+        const spaREPart = await axios.get(
+          "http://localhost:3000/api/spareparts/"
+        );
+        setSparePart(spaREPart.data);
+      } catch (error) {
+        console.error("Error fetching modifications:", error);
+      }
+    };
+    fetchModifications();
+  }, []);
+
   const navigateAddPkg = () => {
     navigate("/admin/add-pkg");
   };
+
+  const openCard = (order) => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  // Define handleOnChange function
+  const handleOnChange = (e, selectedPart) => {
+    const { name, value } = e.target;
+    setItem((prevItem) => ({
+      ...prevItem,
+      [name]: value,
+    }));
+  };
+
+  // Define handleOnSubmit function
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    setPickList((prevList) => [...prevList, item]);
+    setItem({ name: "", quantity: 0 }); // Reset the form fields after submission
+  };
+
+  const handleRequest = async () => {
+    try {
+      const response = await axios.post("http://localhost:3000/api/spreq/add", {
+        item: pickList,
+      });
+      Swal.fire("Request Sent Successfully!", "", "success");
+      window.location.reload();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
 
   return (
     <div className="p-8">
@@ -156,6 +215,8 @@ const ShowAllPackages = () => {
               <th className="py-3 px-5 text-left">Price</th>
               <th className="py-3 px-5 text-left">Services</th>
               <th className="py-3 px-5 text-left">Action</th>
+              <th className="py-3 px-5 text-left">Request</th>
+
             </tr>
           </thead>
           <tbody>
@@ -187,12 +248,114 @@ const ShowAllPackages = () => {
                     Delete
                   </button>
                 </td>
+                <td className="py-3 px-2 text-ExtraDarkColor">
+                  <button
+                    className="bg-green-600 text-white mt-1 ml-2 inline-block px-8 py-2.5 text-sm uppercase rounded-full shadow-lg transition-transform duration-200 ease-in-out hover:-translate-y-1 hover:shadow-lg active:translate-y-px active:shadow-md"
+                    onClick={(e) => openCard(item)}
+                  >
+                    Request Parts
+                  </button>
+                </td>
               </tr>
             ))}
           </tbody>
         </table>
+        <Modal
+          isOpen={isModalOpen}
+          onRequestClose={closeModal}
+          className="text-center bg-white p-10 h-fit w-3/4 max-w-4xl rounded-xl ml-20 "
+          overlayClassName="fixed inset-0 flex justify-center items-center bg-black bg-opacity-64 rounded-xl"
+        >
+          <div className="">
+            <form onSubmit={handleOnSubmit}>
+              <div className=" bg-slate-200 p-6 text-black rounded-2xl shadow-sm">
+                <h1 className="text-3xl font-bold mb-4">Spare Part Request</h1>
+                <div className="flex items-center justify-between mb-4">
+                  <div className="flex flex-col mb-4 w-1/2">
+                    <label className="block text-gray-700 required">
+                      Item Name:
+                    </label>
+                    <select
+                      className="border border-gray-300 text-black rounded-md p-2"
+                      name="name"
+                      value={item.name}
+                      onChange={(e) => {
+                        const selectedPart = sparePart.find(
+                          (part) => part.name === e.target.value
+                        );
+                        handleOnChange(e, selectedPart);
+                      }}
+                      required
+                    >
+                      {sparePart.map((part) => (
+                        <option key={part._id} value={part.name}>
+                          {part.partName}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
+
+                  <div className="flex flex-col mb-4">
+                    <label className="block text-gray-700 required">
+                      Quantity:
+                    </label>
+                    <input
+                      type="number"
+                      className="border border-gray-300 text-black rounded-md p-2 w-2/3"
+                      placeholder="Quantity"
+                      name="quantity"
+                      value={item.quantity}
+                      onChange={handleOnChange}
+                      required
+                    />
+                  </div>
+                </div>
+                <button
+                  type="submit"
+                  className="bg-black text-white px-4 py-2 rounded-md"
+                >
+                  Add To List
+                </button>
+              </div>
+            </form>
+
+            {/* Picklist Table */}
+            <div className="mt-4">
+              {pickList.length <= 0 ? (
+                <p>No items added yet</p>
+              ) : (
+                <table className="min-w-full text-black bg-white border border-gray-300">
+                  <thead>
+                    <tr className="bg-gray-100">
+                      <th className="py-2 px-4 border-b">Name</th>
+
+                      <th className="py-2 px-4 border-b">Quantity</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {pickList.map((item, index) => (
+                      <tr key={index} className="border-b text-center">
+                        <td className="py-2 px-4">{item.name}</td>
+
+                        <td className="py-2 px-4">{item.quantity}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              )}
+
+              <button
+                className="bg-pink-600 text-white mt-10 ml-2 inline-block px-8 py-2.5 text-sm uppercase rounded-full shadow-lg transition-transform duration-200 ease-in-out hover:-translate-y-1 hover:shadow-lg active:translate-y-px active:shadow-md"
+                onClick={handleRequest}
+              >
+                Send Request
+              </button>
+            </div>
+          </div>
+        </Modal>
       </div>
     </div>
+    
   );
 };
 
